0d3b8bbfc4b50ca868b95f32f6b0d67008aa0551
rbufnoir <remi.bufnoir@free.fr>
2022-06-23 13:39:58 +0200

    Formulaire en utilisant bootstrap

--- function.js
+++ function.js
@@ -1,18 +1,31 @@
-function showDiag(){
-    document.getElementById("dateDialog").showModal();
-}
+function createBlur() {
+  var blurStyle = document.createElement("style");
+  blurStyle.innerHTML = ".blur :not(#staticBackdrop, #staticBackdrop *) {filter: blur(1.5rem);color: transparent;text-shadow: 0 0 8px #000;}";
 
-function hideDiag() {
-  document.getElementById("dateDialog").close();
+  document.body.appendChild(blurStyle);
 }
 
 function ageChecker() {
-  const userAge = document.getElementById("dateForm").value;
+  const today = new Date();
+  const birthday = new Date(document.getElementById("dateForm").value);
+  let diff = today.getTime() - birthday.getTime();
 
-  if (userAge >= 18){
-    hideDiag();
-    document.body.classList.remove("blur")
+  if (diff > 3786834240000)
+    alert("Erreur!\nVous êtes censés être mort: êtes-vous un vampire? (si oui merci de contacter la fondation SCP)");
+  else if (diff > 568036800000){
+    $(window).ready(function(){$("#staticBackdrop").modal('hide');});
+    document.body.classList.remove("blur");
   }
+  else if (diff < 0)
+    alert("Erreur!\nParadoxe temporel: vous êtes nés dans le futur");
   else
     location.assign("http://www.google.com");
 }
+
+function initPage () {
+  document.body.classList.add("blur");
+  createBlur();
+  $(window).ready(function(){$("#staticBackdrop").modal('show');});
+}
+
+initPage();
