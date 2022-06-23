d2fc754ac2845dd821bf456b0a66da8bba0562a8
rbufnoir <remi.bufnoir@free.fr>
2022-06-23 09:30:18 +0200

    Gestion des valeurs absurdes

--- function.js
+++ function.js
@@ -11,10 +11,14 @@ function ageChecker() {
   const birthday = new Date(document.getElementById("dateForm").value);
   let diff = today.getTime() - birthday.getTime();
 
-  if (diff > 568036800000){
+  if (diff > 3786834240000)
+    alert("Erreur!\nVous êtes censés être mort: êtes-vous un vampire? (si oui merci de contacter la fondation SCP)");
+  else if (diff > 568036800000){
     hideDiag();
     document.body.classList.remove("blur")
   }
+  else if (diff < 0)
+    alert("Erreur!\nParadoxe temporel: vous êtes nés dans le futur");
   else
     location.assign("http://www.google.com");
 }
